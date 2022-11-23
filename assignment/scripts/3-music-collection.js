50648e910073fb7ab15bc4f27a3807ff7670f9d7
Jeremy Hanson <rockbasser88@hotmail.com>
2022-11-22 21:16:59 -0600

    tested addToCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -9,3 +9,13 @@ function addToCollection(title, artist, yearPublished) {
     collection.push(album);
     return album;
 }
+
+// adding albums
+console.log('Album added:', addToCollection('Kill \'Em All', 'Metallica', 1983));
+console.log('Album added:', addToCollection('Boston', 'Boston', 1976));
+console.log('Album added:', addToCollection('The Foundation', 'Zac Brown Band', 2008));
+console.log('Album added:', addToCollection('Plastic Hearts', 'Miley Cyrus', 2020));
+console.log('Album added:', addToCollection('Black Sails In The Sunset', 'AFI', 1999));
+console.log('Album added:', addToCollection('Death Magnetic', 'Metallica', 2008));
+
+console.log('Music collection:', collection);
