361b60c291a3edc78942f99d2cd008c9f9d57570
Jeremy Hanson <rockbasser88@hotmail.com>
2022-11-22 20:24:54 -0600

    added addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,11 @@
 console.log('***** Music Collection *****')
+
+// establish music collection
+let collection = [];
+
+// adds albums to music collection
+function addToCollection(title, artist, yearPublished) {
+    let album = {title, artist, yearPublished};
+    collection.push(album);
+    return album;
+}
