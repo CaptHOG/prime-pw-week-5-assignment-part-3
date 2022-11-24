59ed592e103a7e730e9b9246d022ff364918d0c8
Jeremy Hanson <rockbasser88@hotmail.com>
2022-11-23 21:18:22 -0600

    added showCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,7 +1,9 @@
 console.log('***** Music Collection *****')
 
+
 // establish music collection
 let collection = [];
+
 
 // adds albums to music collection
 function addToCollection(title, artist, yearPublished) {
@@ -9,6 +11,7 @@ function addToCollection(title, artist, yearPublished) {
     collection.push(album);
     return album;
 }
+
 
 // adding albums
 console.log('Album added:', addToCollection('Kill \'Em All', 'Metallica', 1983));
@@ -18,4 +21,19 @@ console.log('Album added:', addToCollection('Plastic Hearts', 'Miley Cyrus', 202
 console.log('Album added:', addToCollection('Black Sails In The Sunset', 'AFI', 1999));
 console.log('Album added:', addToCollection('Death Magnetic', 'Metallica', 2008));
 
+
+// shows music collection
 console.log('Music collection:', collection);
+
+
+// shows collection of albums
+function showCollection(array) {
+    console.log('Number of albums in collection:', array.length);
+    array.forEach(album => {
+        for (const title in array) {
+            console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`);
+        }
+    });
+}
+// testing showCollection
+showCollection(collection);
